5668913c837a47a5082f0d776463fe2690364c02
Martin Grunwald <martingrunwald91@gmail.com>
2024-02-12 11:47:05 +0000

    edit insall.zsh if /usr/share/bin is not accessible

--- install.zsh
+++ install.zsh
@@ -17,6 +17,6 @@ chmod  +x $LOGIN_SRC/mk_login.py
 if [[ -d /usr/sbin ]]; then
   sudo ln -s $LOGIN_SRC/mk_login.py /usr/sbin/mklogin
   else
-  cp ../.mk_login_inst .mklogin
+  cp ../.mklogin_inst .mklogin
   ln -s $HOME/.mklogin/mk_login.py $HOME/mklogin
 fi
