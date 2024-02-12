26a9ca68ee44c787ef1ba58e97c8aff4bfcb8e59
Martin Grunwald <martingrunwald91@gmail.com>
2024-02-12 07:21:41 +0000

    init

---
+++ install.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+export LOGINS="$HOME/logins"
+export LOGIN_SRC="$HOME/.local/share/mklogin"
+
+if ! [[ -d LOGINS ]]; then
+    mkdir -p LOGINS
+fi
+
+if ! [[ -d LOGIN_SRC ]]; then
+  mkdir -p LOGIN_SRC
+fi
+
+cp ./mk_login.py $LOGIN_SRC/mk_login.py
+
+chmod  +x $LOGIN_SRC/mk_login.py
+sudo ln -s $LOGIN_SRC/mk_login.py /usr/bin/mklogin
--- mk_login.py
+++ mk_login.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
 import  os
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--user',"-u", type=str,
+parser = argparse.ArgumentParser(description='Create ZSH login script for remote computer through secure shell')
+parser.add_argument('--user',"-u", required=True, type=str,
                     help='username')
-parser.add_argument('--host',"-H", type=str,
+parser.add_argument('--host',"-H", required=True, type=str,
                     help='hostname')
 
 
@@ -18,23 +19,30 @@ BASE=$(basename "$0")
 PARSED=(${(s/@/)BASE})
 USER=$PARSED[1]
 HOST=$PARSED[2]; HOST=${HOST/.login/}; HOST=${HOST/.zsh/}
-echo -e "Login to" "\e[32m$USER\e[0m"@"\e[36m$HOST\e[0m"
+
+echo -e ">>> Login to" "\e[32m$USER\e[0m"@"\e[36m$HOST\e[0m >>>"
+
 LOGIN=$USER@$HOST
 
 SSH_ID=$HOME/.ssh/id_$LOGIN
 
-if ! [[ -f $SSH_ID ]] {
-ssh-keygen -t rsa -f $HOME/.ssh/id_$LOGIN -N ""
-ssh-copy-id -i $HOME/.ssh/id_$LOGIN
-}
+if ! [[ -f $SSH_ID ]]; then
+    ssh-keygen -t rsa -f $HOME/.ssh/id_$LOGIN -N "" 1> /dev/null
+    ssh-copy-id -i $SSH_ID $LOGIN &> /dev/null
+fi
+TIME_STAMP_1=$(date +%s)
+ssh -i $SSH_ID $LOGIN
+TIME_STAMP_2=$(date +%s)
 
-ssh "$LOGIN"
+DUR=$(((TIME_STAMP_2 - TIME_STAMP_1)))
+echo -e ">>> Exited from \e[32m$USER\e[0m"@"\e[36m$HOST\e[0m, duration= $DUR sec >>>"
 
-    """
-
-    with open(filename, "w+") as f:
+"""
+    filepath = os.path.join(os.environ["HOME"], "logins", filename)
+    with open(filepath, "w+") as f:
         f.write(script)
-    os.system(f"chmod +x {filename}")
+    os.system(f"chmod +x {filepath}")
+    print(f"File created \033[92m{filepath}\033[0m")
 
 if __name__ == "__main__":
     main(parser.parse_args())
